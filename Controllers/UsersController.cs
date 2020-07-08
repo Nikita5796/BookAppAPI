96675a68ff49f4fef696036fce3063118b1f00fe
Nikita Thorat <NikitaT@citiustech.com>
2020-07-08 11:22:51 +0530

    Changes done

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApp.API.Entities;
+using BookApp.API.Services;
+using BookApp.BLL;
+using BookApp.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserServiceBLL UserObj = new UserServiceBLL(new UserContext());
+
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.EmailId, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpPost("add")]
+        public ActionResult<User> AddUser(User user)
+        {
+            UserObj.AddUserBLL(user);
+
+            return Ok();
+        }
+    }
+}
